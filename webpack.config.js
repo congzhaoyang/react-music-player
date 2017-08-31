72c36aeee3e736866cabe30e22bd35c3a3c47b09
congzhaoyang <congzhaoyang1@163.com>
2017-08-31 23:57:02 +0800

    2017-08-31 push

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,61 @@
-const path = require('path');
-const webpack = require('webpack');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // 自动生成build文件夹及文件：
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
     entry: [
-        'react-hot-loader/patch',
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
-        path.join(__dirname, 'app/index.js')
+        'react-hot-loader/patch',
+        path.join(__dirname, 'app/final/index.js')
     ],
     output: {
-        path: path.join(__dirname, '/build/'),
+        path: path.join(__dirname, '/dist/'),
         filename: '[name].js',
-    },
-    resolve: {
-        extensions: [".js", ".json", ".jsx", ".css","scss"],
-    },
-    module: {
-        rules: [{
-            test: /\.js$/, // babel 转换为兼容性的 js
-            exclude: /node_modules/,
-            use:{
-                loader: 'babel-loader',
-                query: {
-                    presets:['react', 'es2015']
-                }
-            }
-        }, {
-            test: /\.scss$/,
-            use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({ // css-hot-loader结局热替换CSS不自动刷新
-                fallback: 'style-loader',
-                use: ['css-loader', 'sass-loader'],
-            }))
-        }, {
-            test: /\.(png|jpg|gif)$/,
-            use:[{
-                loader: 'url-loader',
-                options: {
-                    limit: 8192 // 小于8KB 使用base64格式图片
-                }
-            }]
-        }, {
-            test: /\.css$/,
-            use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: 'css-loader'
-            }))
-        },{
-            test: /\.(mp3|webm|ogg)/,
-            use: {
-                loader: 'file-loader',
-            }
-        }]
+        publicPath: '/'
     },
     plugins: [
-        //这个使用uglifyJs压缩你的js代码
-        new webpack.optimize.UglifyJsPlugin({ minimize: true }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
         new HtmlWebpackPlugin({
-            title: 'react-music-player',
-            template: './index.html',
-            filename: 'index.html',
-            inject: 'body'
+          template: './app/index.tpl.html',
+          inject: 'body',
+          filename: './index.html'
         }),
-        new ExtractTextPlugin("styles.css"),
-        new OpenBrowserPlugin({ url: 'http://localhost:3000' })
-    ]
-}
+        //new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+    module: {
+        resolve:{
+            extensions:['','.js','.json']
+        },        
+        loaders: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              query:
+                {
+                  presets:['react','es2015']
+                }
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
+    }
+};
